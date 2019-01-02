acd802afbe4e652e2c895c1a656d97b84ed48bce
May <ben.may@mullenloweprofero.com>
2019-01-02 14:33:33 +0000

    update and clear

--- ch11/view.py
+++ ch11/view.py
@@ -2,6 +2,7 @@ from tkinter import *
 import model
 
 cell_size = 5
+is_running = False
 
 def setup():
     global root, grid_view, cell_size, start_button, clear_button, choice
@@ -16,7 +17,9 @@ def setup():
                              bg='white')
 
     start_button = Button(root, text='Start', width=12)
+    start_button.bind('<Button-1>', start_handler)
     clear_button = Button(root, text='Clear', width=12)
+    clear_button.bind('<Button-1>', clear_handler)
 
     choice = StringVar(root)
     choice.set('Choose a Pattern')
@@ -29,6 +32,57 @@ def setup():
     option.grid(row=1, column=1, padx=20)
     clear_button.grid(row=1, column=2, sticky=E, padx=20, pady=20)
 
+def start_handler(event):
+    global is_running, start_button
+
+    if is_running:
+        is_running = False
+        start_button.configure(text='Start')
+    else:
+        is_running = True
+        start_button.configure(text='Pause')
+        update()
+
+def clear_handler(event):
+    global is_running, start_button
+
+    is_running = False
+    for i in range(0, model.height):
+        for j in range(0, model.width):
+            model.grid_model[i][j] = 0
+
+    start_button.configure(text='Start')
+    update()
+
+def update():
+    global grid_view, root, is_running
+
+    grid_view.delete(ALL)
+
+    model.next_gen()
+    for i in range(0, model.height):
+        for j in range(0, model.width):
+            if model.grid_model[i][j] == 1:
+                draw_cell(i, j, 'black')
+
+    if (is_running):
+        root.after(100,update)
+
+def draw_cell(row, col, color):
+    global grid_view, cell_size
+
+    if color == 'black':
+        outline = 'grey'
+    else:
+        outline = 'white'
+
+    grid_view.create_rectangle(row*cell_size,
+                               col*cell_size,
+                               row*cell_size+cell_size,
+                               col*cell_size+cell_size,
+                               fill=color, outline=outline)
+
 if __name__ == '__main__':
     setup()
+    update()
     mainloop()
